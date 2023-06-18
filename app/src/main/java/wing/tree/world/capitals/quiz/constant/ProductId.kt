7a522403c74139acaf93dd975a25de22f5cd7d32
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-06-19 08:49:57 +0900

    feat : Introduce billing library and declare basic properties

---
+++ app/src/main/java/wing/tree/world/capitals/quiz/constant/ProductId.kt
@@ -0,0 +1,5 @@
+package wing.tree.world.capitals.quiz.constant
+
+object ProductId {
+    const val remove_ads = "remove_ads"
+}
---
+++ app/src/main/java/wing/tree/world/capitals/quiz/constant/Products.kt
@@ -0,0 +1,10 @@
+package wing.tree.world.capitals.quiz.constant
+
+import com.wing.tree.bruni.billing.model.Product
+
+val products = listOf(
+    Product.INAPP(
+        id = ProductId.remove_ads,
+        consumable = false
+    )
+)
