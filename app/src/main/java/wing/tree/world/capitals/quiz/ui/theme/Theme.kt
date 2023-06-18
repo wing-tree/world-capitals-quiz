c02c7631540d0fe4873f7e1ec7fcc402cfd548da
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-06-18 22:58:47 +0900

    refactor : Cleanup

--- app/src/main/java/wing/tree/world/capitals/quiz/ui/theme/Theme.kt
+++ app/src/main/java/wing/tree/world/capitals/quiz/ui/theme/Theme.kt
@@ -1,7 +1,6 @@
 package wing.tree.world.capitals.quiz.ui.theme
 
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 
