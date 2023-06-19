d4d1e2a2f60b10f8059cb3f11822012fb2726746
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-06-19 20:49:07 +0900

    feat : Add new color

--- app/src/main/java/wing/tree/world/capitals/quiz/ui/theme/Color.kt
+++ app/src/main/java/wing/tree/world/capitals/quiz/ui/theme/Color.kt
@@ -3,6 +3,7 @@ package wing.tree.world.capitals.quiz.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val CloudWhite = Color(0xFFF2F1E6)
+val FacebookBlue = Color(0xFF1877F2)
 val RedditRed = Color(0xFFFF4500)
 val SkyBlue = Color(0xFF87CEEB)
 val SpanishSkyBlue = Color(0xFF00AAE4)
